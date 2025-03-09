3735a5abeaed1a48d54c9328768aa9e90c84b968
mmohankowshik <160257017+mmohankowshik@users.noreply.github.com>
2025-03-09 16:10:17 +0530

    Delete linear_search.py

--- linear_search.py
+++
@@ -1,8 +0,0 @@
-list= [10,14,19,25,27,31,34,43,45,52]
-n = int(input('enter your key value:'))
-for n in list:
-    if n  in list:
-        print('success! element found')
-        break
-    else:
-        print('failure! element not found')
